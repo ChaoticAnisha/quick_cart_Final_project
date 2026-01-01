96983584b7c79ef72a67a45f78c48e1212b3f6c0
ChaoticAnisha <anishashah0117>
2026-01-01 14:17:39 +0545

    initialize Hive and open users/session boxes at startup

--- lib/data/repositories/auth_repository_impl.dart
+++ lib/data/repositories/auth_repository_impl.dart
@@ -148,11 +148,7 @@ class AuthRepositoryImpl implements AuthRepository {
 }
 
 class     AuthException implements Exception {
-  final String message;
-  AuthException(this.message);
-
-  @override
-  String toString() => 'AuthException: $message';
+  
 }
 
 
