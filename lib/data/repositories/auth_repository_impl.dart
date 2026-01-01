0d0f9690dad1fdbf165ed783adec96495893962a
ChaoticAnisha <anishashah0117>
2026-01-01 14:24:29 +0545

    route to dashboard if session exists

--- lib/data/repositories/auth_repository_impl.dart
+++ lib/data/repositories/auth_repository_impl.dart
@@ -146,10 +146,7 @@ class AuthRepositoryImpl implements AuthRepository {
   }
 }
 class AuthException implements Exception {
-  final String message;
-  AuthException(this.message);
-  @override
-  String toString() => 'AuthException: $message';     
+       
 }
 
 
